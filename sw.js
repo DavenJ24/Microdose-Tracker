aaba42f4962ee122423b9a1292d44d4a789ec853
DavenJ24 <davenjohnson1998@gmail.com>
2025-09-05 05:26:12 -0500

    d
    d

--- sw.js
+++
@@ -1,6 +0,0 @@
-// SW v1.3.1 static
-const CACHE='echosage-static-131';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))).then(()=>self.clients.claim())});
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>{if(e.request.mode==='navigate')return caches.match('./index.html')})))});
